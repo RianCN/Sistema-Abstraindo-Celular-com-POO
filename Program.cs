97967362a2e0dfb1b20125be72af4ba54b6ac717
Rian-CN <riancosta20@outlook.com>
2023-12-20 19:43:06 -0300

    Desafio Concluido.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,28 @@
 ﻿using DesafioPOO.Models;
+using System;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Iphone:");
+            Iphone iphone = new Iphone("987654321", "iPhone Model", "IMEI987", 64);
+            iphone.Ligar();
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("Netflix");
+
+            Console.ReadLine();
+
+            Console.WriteLine("Nokia:");
+            Nokia nokia = new Nokia("123456789", "Nokia Model", "IMEI123", 16);
+            nokia.Ligar();
+            nokia.ReceberLigacao();
+            nokia.InstalarAplicativo("YouTube");
+
+            Console.WriteLine();
+        }
+    }
+}
